5cd6197e6817bcafdb206c22af457cf4eac88ce0
Ytshak_Raiyner <yr3275744@gmail.com>
2026-05-27 11:06:03 +0300

    add first version of Circle class

--- circle.py
+++ circle.py
@@ -1,2 +1,26 @@
-class Circle:
-    pass
+from shape import Shape
+from math import pi
+
+class Circle(Shape):
+    """docstring"""
+    def __init__(self, shape_id, radius):
+        """docstring"""
+        super().__init__(shape_id, "circle")
+        self.radius = radius
+    
+    def get_area(self):
+        """docstring"""
+        return (self.radius ** 2) * pi
+    
+    def get_permiter(self):
+        """docstring"""
+        return 2 * self.radius * pi
+    
+    def to_dict(self):
+        """docstring"""
+        the_shape = {"id":self.id, "type":self.shape_type, "radius":self.radius}
+        return the_shape
+    
+if __name__ == "__main__":
+    a_circ = Circle(4321, 20)
+    print(a_circ.get_permiter())
