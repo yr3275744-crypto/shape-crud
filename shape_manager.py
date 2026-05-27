eac6c188a9907d6af6458ae7a18686ba00c3d62a
Ytshak_Raiyner <yr3275744@gmail.com>
2026-05-27 12:12:26 +0300

    add ShapeManager class

---
+++ shape_manager.py
@@ -0,0 +1,30 @@
+class ShapeManager:
+    """docstring"""
+    def __init__(self):
+        """docstring"""
+        self.shapes = []
+        self.load_from_json()
+    
+    def create_shape(self, shape):
+        """docstring"""
+        pass
+    
+    def get_all_shapes(self):
+        """docstring"""
+        pass
+    
+    def update_shape(self, shape_id, new_data):
+        """docstring"""
+        pass
+    
+    def delete_shape(self, shape_id):
+        """docstring"""
+        pass
+    
+    def save_to_json(self):
+        """docstring"""
+        pass
+    
+    def load_from_json(self):
+        """docstring"""
+        pass
