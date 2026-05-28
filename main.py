75e0170b133c687d42f2dc860b0982a6d441a291
Ytshak_Raiyner <yr3275744@gmail.com>
2026-05-29 00:26:50 +0300

    add add_shape_handle function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import shape_manager
+#TODO:add logs to all files. add checks to load_from_json methods in shape_manager
+
+from shape_manager import ShapeManager
 
 def print_menu() -> None:
     """docstring"""
@@ -12,7 +14,7 @@ def print_menu() -> None:
     return None
 
 
-def get_user_coice() -> int:
+def get_user_choice() -> int:
     """docstring"""
     try:
         user_choice = int(input("Enter your choice: "))
@@ -26,33 +28,74 @@ def get_user_coice() -> int:
 
 
 def create_circle_dict() -> dict:
-    try:
-        radius = float(input("Enter the radius: "))
-        return {"type": "circle", "radius":radius}
-    
-    except ValueError:
-        raise ValueError("The radius value is invalid.")
+    """docstring"""
+    radius = float(input("Enter the radius: "))
+    return {"type": "circle", "radius":radius}
 
 
 def create_square_dict() -> dict:
-    try:
-        side = float(input("Enter the side value: "))
-        return {"type": "square", "side":side}
-    
-    except ValueError:
-        raise ValueError("The side value is invalid.")
+    """docstring"""
+    side = float(input("Enter the side value: "))
+    return {"type": "square", "side":side}
     
 
 def create_rectangle_dict() -> dict:
+    """docstring"""
+    length_side = float(input("Enter the length side value: "))
+    width_side =  float(input("Enter the width side value: "))
+    return {"type": "square", "length_side":length_side, "width_side":width_side}
+    
+
+
+
+def add_shape_handle(shape_manager:ShapeManager, file_path:str, create_dict_functions:dict) -> None:
+    """docstring"""
     try:
-        length_side = float(input("Enter the length side value: "))
-        width_side =  float(input("Enter the width side value: "))
-        return {"type": "square", "length_side":length_side, "width_side":width_side}
+        shape_name = input("Enter the sape you want add: ")
+        shape_dict = create_dict_functions(shape_name)
+    
+    except KeyError:
+        raise("The shape does not exists.")
     
     except ValueError:
-        raise ValueError("The side value is invalid.")
+        raise ValueError("The value is invalid.")
+
+    shape_object = shape_manager.create_shape(shape_dict)
+    shape_manager.add_shape(shape_object)
+    shape_manager.save_to_json(file_path)
+    
+    return None
+
+def main(json_file_path:str) -> None:
+    """docstring"""
+    create_dict_functions = {"circle":create_circle_dict, 
+                                   "square":create_square_dict, 
+                                   "rectangle":create_rectangle_dict}
+    
+    shape_manager = ShapeManager(json_file_path)
+    user_choice = None
+
+    while user_choice != 5:
+        print_menu()
+        
+        try:
+            user_choice = get_user_choice()
+        
+        except ValueError as e:
+            print(e)
+            continue
+        
+        match user_choice:
+            case 1:
+                try:
+                    add_shape_handle(shape_manager, json_file_path, create_dict_functions)
+                except (KeyError, ValueError) as e:
+                    print(e)
+                    continue
+            
+                   
 
 
 
-def main():
-    pass
+if __name__ == "__main__":
+    main("shapes.json")
