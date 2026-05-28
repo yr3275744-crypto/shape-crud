5e6ca13dac8127050cef09bd8e1835881900929a
Ytshak_Raiyner <yr3275744@gmail.com>
2026-05-28 10:57:36 +0300

    add create dictionary functions to main.py

--- main.py
+++ main.py
@@ -25,8 +25,33 @@ def get_user_coice() -> int:
         raise e("You must enter a number between 1 and 5.")
 
 
-# def create_circle_dict():
-#     radius = float(input("Enter the radius: "))
+def create_circle_dict() -> dict:
+    try:
+        radius = float(input("Enter the radius: "))
+        return {"type": "circle", "radius":radius}
+    
+    except ValueError:
+        raise ValueError("The radius value is invalid.")
+
+
+def create_square_dict() -> dict:
+    try:
+        side = float(input("Enter the side value: "))
+        return {"type": "square", "side":side}
+    
+    except ValueError:
+        raise ValueError("The side value is invalid.")
+    
+
+def create_rectangle_dict() -> dict:
+    try:
+        length_side = float(input("Enter the length side value: "))
+        width_side =  float(input("Enter the width side value: "))
+        return {"type": "square", "length_side":length_side, "width_side":width_side}
+    
+    except ValueError:
+        raise ValueError("The side value is invalid.")
+
 
 
 def main():
