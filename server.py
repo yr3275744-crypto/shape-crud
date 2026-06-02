6e44d0ee5dc8594dd894351ee1c1e4280851beac
Ytshak_Raiyner <yr3275744@gmail.com>
2026-06-02 11:26:16 +0300

    add to server.py get shape function

--- server.py
+++ server.py
@@ -1,18 +1,24 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from pydantic import BaseModel
-
 from shape_manager import ShapeManager
 
 def manege_app():
     app = FastAPI()
     shape_manager = ShapeManager("shapes.json")
 
-
     @app.get("/shapes")
     def get_shapes():
         return shape_manager.get_all_shapes()
 
+    @app.get("/shapes/{id}")
+    def get_Shape_by_id(id:int):
+        shape = shape_manager.find_shape_by_id(id)
+        if shape:
+            return shape.to_dict()
+        else:
+            
+            return status.HTTP_404_NOT_FOUND
 
     uvicorn.run(app)
 
